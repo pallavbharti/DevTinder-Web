9b1ea973562fff2268da336f8ef9d0fba77b5348
pallavbharti <pallavbharti8795@gmail.com>
2026-07-09 12:39:21 +0530

    feat: add dark/light mode toggle

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -22,12 +22,44 @@ const NavBar = () => {
       console.error("Logout error:", error);
     }
   };
-
+  const handleThemeToggle = (e) => {
+  const theme = e.target.checked ? "dark" : "light";
+  document.documentElement.setAttribute("data-theme", theme);
+  };
   return (
     <div className="navbar bg-base-300">
       <div className="flex-1">
         <Link to="/" className="btn btn-ghost text-xl">🧑‍💻 DevTinder</Link>
       </div>
+      <label className="flex cursor-pointer gap-2">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="20"
+    height="20"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round">
+    <circle cx="12" cy="12" r="5" />
+    <path
+      d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
+  </svg>
+  <input type="checkbox" value="synthwave" className="toggle theme-controller" onChange={handleThemeToggle} />
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="20"
+    height="20"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round">
+    <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+  </svg>
+      </label>
       {user && (
         <div className="flex-none gap-2 mx-5 flex items-center">
           <p>Welcome, {user.firstName}</p>
